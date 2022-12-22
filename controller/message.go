5ba1f0928a63ccb9ed6cca640c9c5684320f3987
JustSong <quanpengsong@gmail.com>
2022-12-23 00:36:56 +0800

    feat: support custom URL

--- controller/message.go
+++ controller/message.go
@@ -113,20 +113,20 @@ func pushMessageHelper(c *gin.Context, message *model.Message) {
 			message.Channel = channel.TypeEmail
 		}
 	}
-	if message.URL == "" {
-		if common.MessagePersistenceEnabled {
-			err = message.UpdateAndInsert(user.Id)
-			if err != nil {
-				c.JSON(http.StatusOK, gin.H{
-					"success": false,
-					"message": err.Error(),
-				})
-				return
-			}
-			message.URL = fmt.Sprintf("%s/message/%s", common.ServerAddress, message.Link)
-		} else {
-			message.URL = fmt.Sprintf("%s/message/unsaved", common.ServerAddress)
+	link := "unsaved"
+	if common.MessagePersistenceEnabled {
+		err = message.UpdateAndInsert(user.Id)
+		if err != nil {
+			c.JSON(http.StatusOK, gin.H{
+				"success": false,
+				"message": err.Error(),
+			})
+			return
 		}
+		link = message.Link
+	}
+	if message.URL == "" {
+		message.URL = fmt.Sprintf("%s/message/%s", common.ServerAddress, link)
 	}
 	err = channel.SendMessage(message, &user)
 	if err != nil {
