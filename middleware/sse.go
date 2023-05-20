c59f758f2cf194bc6490d7e48fe5da3c74d0d1c8
JustSong <songquanpeng@foxmail.com>
2023-05-20 11:43:17 +0800

    fix: add X-Accel-Buffering header on SSE response

--- middleware/sse.go
+++ middleware/sse.go
@@ -8,6 +8,7 @@ func SetSSEHeaders() func(c *gin.Context) {
 		c.Writer.Header().Set("Cache-Control", "no-cache")
 		c.Writer.Header().Set("Connection", "keep-alive")
 		c.Writer.Header().Set("Transfer-Encoding", "chunked")
+		c.Writer.Header().Set("X-Accel-Buffering", "no")
 		c.Next()
 	}
 }
